8528965b3aebfd3103cda1a88018e5195ed6afa0
AdityaMitti <45398989+AdityaMitti@users.noreply.github.com>
2018-12-05 10:45:33 -0500

    partner b
    after bugs

--- DiceHolder.java
+++ DiceHolder.java
@@ -1,25 +1,29 @@
 import java.util.ArrayList;
 public class DiceHolder{
-	private ArrayList<Integer> Holder;
-	public DiceHolder(ArrayList<Integer> HolderI){
+	private ArrayList<Die> Holder;
+	public DiceHolder(ArrayList<Die> HolderI){
 		Holder = HolderI;
 	}
-	public int addDie(){
-		int i = 1;
+	public int addDie(Die d){
 		if(Holder.size() > 5){
 			return -1;
 		}
 		else{
-			Holder.add(i);
+			Holder.add(d);
 			return 1;
 		}
 	}
 	public void shake(){
-		roll();
+		for(int i = 0; i < Holder.size(); i++){
+					Holder.get(i).roll();
+		}
+
 	}
 	public String toString(){
+		String h = "";
 		for (int i = 0; i < Holder.size(); i++){
-			return Holder.get(i)+"\n";
+			h += Holder.get(i)+"\n";
 		}
+	return h;
 	}
 }
